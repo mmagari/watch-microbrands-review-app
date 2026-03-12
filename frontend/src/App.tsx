a2756c8902f52284e709a30a9b62893d15990b0d
Jarek.Mankiewicz <mmagari@wp.pl>
2026-03-12 15:51:07 +0100

    expand SCSS variables with spacing, radius and typography tokens

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage/HomePage";
 import { BrandDetailsPage } from "./pages/BrandDetailsPage/BrandDetailsPage";
-import { Header } from "./components/Header";
+import { Header } from "./components/Header/Header";
 
 function App() {
   return (
     <BrowserRouter>
 
       <Header />
-      
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/brand/:id" element={<BrandDetailsPage />} />
