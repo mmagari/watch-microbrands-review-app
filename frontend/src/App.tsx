b2fb59d102573e679efd9daa0cb08c3f3e778835
Jarek.Mankiewicz <mmagari@wp.pl>
2026-06-04 22:02:42 +0200

    add ArticleDetailsPage to routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { HashRouter, Routes, Route } from "react-router-dom";import { HomePage } from "./pages/HomePage/HomePage";
 import { BrandDetailsPage } from "./pages/BrandDetailsPage/BrandDetailsPage";
 import { ArticlesPage } from "./pages/ArticlesPage/ArticlesPage";
+import { ArticleDetailsPage } from "./pages/ArticlesPage/ArticlesDetailsPage";
 import { Header } from "./components/Header/Header";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/brand/:id" element={<BrandDetailsPage />} />
         <Route path="/articles" element={<ArticlesPage />} />
+        <Route path="/articles/:articleId" element={<ArticleDetailsPage />} />
       </Routes>
     </HashRouter>
   );
